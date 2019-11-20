b5254725a9e3f1296569784512632b18f4c4b006
stefanos athanasoulias <stefanos.athanasoulas@ncryptedcloud.com>
2019-11-21 01:37:19 +0200

    Update observable usage component

--- src/main/components/observablesInUse.js
+++ src/main/components/observablesInUse.js
@@ -14,22 +14,20 @@ myModule.observablesInUse = function (root, j, dir, filename, filesArray, index,
     const rxjsCalls = root.find(j.Identifier)
     let importedCalled = [];
     let showObservableUsed = [];
-    let count = 0;
+    let count = 1;
     let source = "";
     let matchesObservable = []
+    let tempObservableCalled = 0;
 
     //Find how many identifiers we import from the rxjs library
     rxjsCalls.forEach(p => {
         if (p.parentPath.parentPath.node.type == "ImportDeclaration") {
             source = p.parentPath.parentPath.node.source.value
-            if ((source.includes("rxjs")) && (!importedCalled.includes(p.node.name))) {
+            if ((source.includes("rxjs")) && (!importedCalled.includes(p.node.name))) { 
                 if (p.node.name == "Observable") {
                     importedCalled.push(p.node.name)
                 }
             }
-            // if ((source.includes("rxjs/operators")) && (!importedOperators.includes(p.node.name))) {
-            //     importedOperators.push(p.node.name)
-            // }
         }
     })
     //Push the head titles only on first file
@@ -49,42 +47,51 @@ myModule.observablesInUse = function (root, j, dir, filename, filesArray, index,
             let tempData = data.toString().replace("\r\n", "    ");
             let arr = tempData.split(/\r?\n/) //white spaces must match length of string
             arr.forEach(line => {
-                if (containsWord(line, "Observable") && containsWord(line, "new")) {
+                if (containsWord(line, "Observable") && containsWord(line, "new") && containsWord(line, "const")) {
                         let indexOfConst = line.indexOf("const");
                         let indexOfEqual = line.indexOf("=")
                         let tempVariable = line.slice((indexOfConst + 5), indexOfEqual).trim();
-                        console.log(tempVariable)
-
                         matchesObservable.push(tempVariable);
-                } else if (containsWord(line, "Observable") && !containsWord(line, "import") && !containsWord(line, "//")) {
-                    console.log(line)
-                    showObservableUsed.push({
-                        observableVar: "Observable Constructor",
-                        observableCalled: count++,
-                        file: dir
-                    })
+                } else if (containsWord(line, "Observable") && containsWord(line, "new") && containsWord(line, "return")) {
+                    let tempVariable = "Observable";
+                    matchesObservable.push(tempVariable);
                 }
-            
+                // else if (containsWord(line, "Observable") && !containsWord(line, "import") && !containsWord(line, "//")) {
+                //     tempObservableCalled++
+                // }
             });
-
-            matchesObservable.forEach(observable => {
-
+         
+            matchesObservable.map(observable => {
                 count = data.toString().split(observable).length - 1
                 if (data.includes(observable)) {
-                    showObservableUsed.push({
-                        observableVar: observable,
-                        observableCalled: count - 1,
-                        file: dir
-                    })
+                    if(observable == "Observable") {
+                        showObservableUsed.push({
+                            observableVar: "Observable_without_variable",
+                            observableCalled: count - 1,
+                            file: dir
+                        })
+                    } else {
+                        showObservableUsed.push({
+                            observableVar: observable,
+                            observableCalled: count - 1,
+                            file: dir
+                        })
+                    }
                 }
             })
-            // console.log("Operators found in " + dir + " ");
-
+            // if(showObservableUsed.length < 2) {
+            //     showObservableUsed.push({
+            //         observableVar: "Observable_Constructor",
+            //         observableCalled: tempObservableCalled,
+            //         file: dir
+            //     })
+            // } 
             //Push the array of objects for csv export 
             Array.prototype.push.apply(csvRows.rows, showObservableUsed);
 
             //When we come down to the last file to scan we aggregate all the results in order to export them into a csv file
             if (index == (filesArray.length - 1)) {
+                
                 converter.json2csv(csvRows.rows, csvModule.json2csvCallback, {
                     prependHeader: false // removes the generated header of "value1,value2,value3,value4" (in case you don't want it)
                 });
@@ -96,14 +103,22 @@ myModule.observablesInUse = function (root, j, dir, filename, filesArray, index,
                 console.log(res);
             }
         });
+        function changeDesc( value, desc ) {
+            for (var i in showObservableUsed) {
+              if (showObservableUsed[i].value == value) {
+                showObservableUsed[i].desc = desc;
+                 break; //Stop this loop, we found it!
+              }
+            }
+         }
     } catch (err) {
         console.log(err)
     }
 
     if (importedCalled.length > 0) {
-        console.log(`File has imported : ` + importedCalled.length + "  subject observable.");
+        console.log(`File has imported : ` + importedCalled.length + "  observable constructor.");
     } else {
-        console.log("File doesn't not include  rxjs calls")
+        console.log("File doesn't not include  rxjs observables constructors")
     }
 
 };
