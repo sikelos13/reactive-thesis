c9c7a180b7689fb7b4142139f661ac5e2a85702b
stefanos athanasoulias <stefanos.athanasoulas@ncryptedcloud.com>
2019-11-25 00:19:15 +0200

    Subject component updated

--- src/main/components/subjectInUse.js
+++ src/main/components/subjectInUse.js
@@ -11,97 +11,138 @@ function containsWord(str, word) {
 }
 
 myModule.subjectInUse = function (root, j, dir, filename, filesArray, index, csvRows) {
-    const rxjsCalls = root.find(j.Identifier)
-    let importedCalled = [];
+    const rxjsImportDeclarations = root.find(j.Identifier)
+    let importedVariable = [];
+    let importedCalledWithAlias = [];
     let showSubjectUsed = [];
     let count = 0;
-    let source = "";
-    let matchesSubjects = []
+    let newCount = 0;
+    let variableCount = 0;
+    let uniqueAlias = []
+    let uniqueVariables = [];
 
     //Find how many identifiers we import from the rxjs library
-    rxjsCalls.forEach(p => {
-        if (p.parentPath.parentPath.node.type == "ImportDeclaration") {
-            source = p.parentPath.parentPath.node.source.value
-            if ((source.includes("rxjs")) && (!importedCalled.includes(p.node.name))) {
-                if (p.node.name == "Subject") {
-                    importedCalled.push(p.node.name)
-                }
+    rxjsImportDeclarations.forEach(p => {
+        if (p.parentPath.parentPath.node.type == "ImportDeclaration" && p.parentPath.parentPath.node.source.value == "rxjs") {
+            // console.log(p)
+            if ((p.parentPath.value.imported.name !== p.parentPath.value.local.name) && (p.parentPath.value.local.name == "Subject")) {
+                importedCalledWithAlias.push(p.parentPath.value.imported.name);
             }
-            // if ((source.includes("rxjs/operators")) && (!importedOperators.includes(p.node.name))) {
-            //     importedOperators.push(p.node.name)
-            // }
         }
     })
+
+    uniqueAlias = [...new Set(importedCalledWithAlias)];
+
     //Push the head titles only on first file
     if (index == 0) {
         //Initialize array with columns titles
         showSubjectUsed.push({
-            subject: "Subject variable",
+            subjectVar: "Subject variable",
             subjectCalled: "Times Used",
             file: "Found in file:"
         })
     }
-
     //iterate the imported identifiers  and scan files for operators
     try {
-        fs.readFile(dir, (err, data) => {
-            if (err) throw err;
-            // console.log(data.toString())
-            let tempData = data.toString().replace("\r\n", "    ");
-            let arr = tempData.split(/\r?\n/) //white spaces must match length of string
-            let forEachCounter = 0;
-            arr.forEach(line => {
-                if (containsWord(line, "Subject")) {
-                    if (forEachCounter > 0) {
-                        let indexOfConst = line.indexOf("const");
-                        let indexOfEqual = line.indexOf("=")
-                        let tempVariable = line.slice((indexOfConst + 5), indexOfEqual).trim();
-                        console.log(tempVariable)
+        if (uniqueAlias.length < 1) {
+            rxjsImportDeclarations.forEach(nodeObservable => {
+                // console.log(nodeObservable.parentPath.value.type)
 
-                        matchesSubjects.push(tempVariable);
+                if (nodeObservable.value.name == "Subject" && nodeObservable.parentPath.parentPath.node.type !== "ImportDeclaration" && nodeObservable.parentPath.value.type == "NewExpression") {
+                    newCount++;
+                } 
+                 if (nodeObservable.parentPath.parentPath.node.type !== "ImportDeclaration" && nodeObservable.parentPath.parentPath.value.type == "AssignmentExpression") {
+                    // console.log(nodeObservable.parentPath.parentPath.value);
+
+                    if(nodeObservable.parentPath.parentPath.value.right.type == "NewExpression" && nodeObservable.parentPath.parentPath.value.right.callee.name== "Subject" && nodeObservable.parentPath.parentPath.value.right.callee.type== "Identifier") {
+                        // console.log(nodeObservable.parentPath.parentPath.value);
+
+                        if (nodeObservable.parentPath.parentPath.value.left.type == "MemberExpression") {
+                            if (nodeObservable.parentPath.parentPath.value.left.property.type == "Identifier") {
+                            // console.log(nodeObservable.parentPath.parentPath.value.left.property.name);
+                            importedVariable.push(nodeObservable.parentPath.parentPath.value.left.property.name);
+                            //continue implementing the idea of varaibles
+                            }
+                        }
                     }
-                    forEachCounter = +1;
-                }
-            });
-
-            matchesSubjects.forEach(subject => {
-
-                count = data.toString().split(subject).length - 1
-                if (data.includes(subject)) {
-                    showSubjectUsed.push({
-                        subject: subject,
-                        subjectCalled: count - 1,
-                        file: dir
-                    })
                 }
             })
-            // console.log("Operators found in " + dir + " ");
 
-            //Push the array of objects for csv export 
-            Array.prototype.push.apply(csvRows.rows, showSubjectUsed);
+        } else {
+            uniqueAlias.forEach(alias => {
+                rxjsImportDeclarations.forEach(nodeObservable => {
+                    // console.log(nodeObservable.parentPath.value.type)
+                    if (alias == nodeObservable.value.name && nodeObservable.parentPath.parentPath.node.type !== "ImportDeclaration") {
+                        count++;
+                    }
+                    if (nodeObservable.value.name == "Subject" && nodeObservable.parentPath.parentPath.node.type !== "ImportDeclaration" && nodeObservable.parentPath.value.type == "NewExpression") {
+                        newCount++;
+                    }
+                })
+               
+                if (count > 0) {
+                    showSubjectUsed.push({
+                        subjectVar: alias,
+                        subjectCalled: count,
+                        file: dir
+                    })
+                    count = 0;
+                }
+            })
+        }
 
-            //When we come down to the last file to scan we aggregate all the results in order to export them into a csv file
-            if (index == (filesArray.length - 1)) {
-                converter.json2csv(csvRows.rows, csvModule.json2csvCallback, {
-                    prependHeader: false // removes the generated header of "value1,value2,value3,value4" (in case you don't want it)
-                });
+        uniqueVariables = [...new Set(importedVariable)];
 
-                //iterate into csvRows in order to console log specific results.
-                let tempConsoleArray = csvRows.rows
-                let res = alasql('SELECT subject, SUM(subjectCalled) AS subjectCalled FROM ? GROUP BY subject', [tempConsoleArray]);
-                res.shift();
-                console.log(res);
-            }
-        });
+        uniqueVariables.forEach(variable => {
+            rxjsImportDeclarations.forEach(nodeObservable => {
+                if (variable == nodeObservable.value.name && nodeObservable.parentPath.parentPath.node.type !== "ImportDeclaration") {
+                    // console.log(nodeObservable.value)
+                    variableCount++;
+                }
+            })
+            showSubjectUsed.push({
+                subjectVar: variable,
+                subjectCalled: variableCount,
+                file: dir
+            })
+            newCount = 0;
+        })
+
+        if (newCount > 0) {
+            showSubjectUsed.push({
+                subjectVar: "Subject_without_variable",
+                subjectCalled: newCount,
+                file: dir
+            })
+            newCount = 0;
+        }
+
+        //Push the array of objects for csv export 
+        Array.prototype.push.apply(csvRows.rows, showSubjectUsed);
+
+        //When we come down to the last file to scan we aggregate all the results in order to export them into a csv file
+        if (index == (filesArray.length - 1)) {
+
+            converter.json2csv(csvRows.rows, csvModule.json2csvCallback, {
+                prependHeader: false // removes the generated header of "value1,value2,value3,value4" (in case you don't want it)
+            });
+
+            //iterate into csvRows in order to console log specific results.
+            let tempConsoleArray = csvRows.rows
+            let res = alasql('SELECT subjectVar, SUM(subjectCalled) AS subjectCalled FROM ? GROUP BY subjectVar', [tempConsoleArray]);
+            res.shift();
+            console.log(res);
+        }
+
     } catch (err) {
         console.log(err)
     }
 
-    if (importedCalled.length > 0) {
-        console.log(`File has imported : ` + importedCalled.length + "  subject observable.");
-    } else {
-        console.log("File doesn't not include  rxjs calls")
-    }
+    // if (importedCalled.length > 0) {
+    //     console.log(`File has imported : ` + importedCalled.length + "  observable constructor.");
+    // } else {
+    //     console.log("File doesn't not include  rxjs observables constructors")
+    // }
 
 };
 
